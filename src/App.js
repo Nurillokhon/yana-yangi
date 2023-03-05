15727ef6a057d63576f5e90bcfe6adf3c2dbbf39
Rayhona <rdjalilova78@gmail.com>
2023-03-05 09:22:52 +0500

    Navbar Header qismni alohida qildim

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './companent/Header';
 import Footer from './companent/Footer';
+import Navbar from './companent/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <Header/>
      <Footer/>
     </div>
