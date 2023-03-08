ca720b50fcfd593c997338b5890370519362d014
Rayhona <rdjalilova78@gmail.com>
2023-03-08 21:04:19 +0500

    dcbnxxsbdn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Footer from "./companent/footer/Footer";
 import Navbar from "./companent/navbar/Navbar";
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Outlet />
+      <Footer/>
     </div>
   );
 }
---
+++ src/companent/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='row'>
+                <div className="col-3"></div>
+                <div className="col-3"></div>
+                <div className="col-3"></div>
+                <div className="col-3"></div>
+                <div className="col-3"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
